dbbab3bafb0a3f7c9a0407ac0f99c7dae36ea4be
Ranga Rao Karanam <in28minutes@gmail.com>
2019-03-09 17:25:12 +0530

    Thank You for Choosing to Learn from in28Minutes

--- frontend/todo-app/src/components/todo/TodoComponent.jsx
+++ frontend/todo-app/src/components/todo/TodoComponent.jsx
@@ -1,9 +1,45 @@
 import React, {Component} from 'react'
+import moment from 'moment'
+import { Formik, Form, Field } from 'formik';
 
 class TodoComponent extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            id : this.props.match.params.id,
+            description : 'Learn Forms',
+            targetDate : moment(new Date()).format('YYYY-MM-DD')
+        }
+    }
+
     render() {
-        return <div>
-                Todo Component for id - {this.props.match.params.id}</div>
+        return (
+            <div>
+                <h1>Todo</h1>
+                <div className="container">
+                    <Formik>
+                        {
+                            (props) => (
+                                <Form>
+                                    <fieldset className="form-group">
+                                        <label>Description</label>
+                                        <Field className="form-control" type="text" name="description"/>
+                                    </fieldset>
+                                    <fieldset className="form-group">
+                                        <label>Target Date</label>
+                                        <Field className="form-control" type="date" name="targetDate"/>
+                                    </fieldset>
+                                    <button className="btn btn-success" type="submit">Save</button>
+                                </Form>
+                            )
+                        }
+                    </Formik>
+                
+                </div>                
+            </div>
+        )
     }
 }
 
